ba4d5494dd7097d8fa8a302f7ddcc280c56878d3
jomapormentilla <jomapormentilla@gmail.com>
2021-04-26 13:17:14 -0400

    Install Dotenv-Rails & JWT gems. Complete JWT implementation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
+    def index
+        users = User.all
+        render json: UserSerializer.new(users).to_serialized_json
+    end
+
     def profile
         render json: UserSerializer.new(current_user).to_serialized_json
+    end
+
+    def create
+        user = User.create(user_params)
+
+        if user.valid?
+            token = encode_token(user_id: user.id)
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
+        else
+            render json: { error: 'Failed to create user.' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :email,
+            :password,
+            :account_type,
+            :status,
+            :job_search_start_date
+        )
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :weeks
   resources :activities
   resources :users
+
+  post '/login' => "auth#create"
+  get '/profile' => "users#profile"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
