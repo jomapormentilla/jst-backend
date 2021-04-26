f703b76ed20a7cd47eb86f602c54ca3574c32c59
jomapormentilla <jomapormentilla@gmail.com>
2021-04-26 12:39:53 -0400

    Implement JWT authentication logic.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def profile
+        render json: UserSerializer.new(current_user).to_serialized_json
+    end
 end
---
+++ app/services/user_serializer.rb
@@ -0,0 +1,16 @@
+class UserSerializer
+    def initialize(user_object)
+        @user = user_object
+    end
+
+    def to_serialized_json
+        @user.to_json(
+            :include => [
+                
+            ],
+            :except => [
+                :password_digest, :token
+            ]
+        )
+    end
+end
