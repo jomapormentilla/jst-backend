1ba80e4ba265a5a54bab95f8bcff74ade3b69125
Shilcof <robert.shilcof@gmail.com>
2021-04-27 19:59:14 +0100

    adds password test to user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,19 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase 
 
     test "is nameable" do
-        test = User.new
-        test.name = "John Smith"
-        assert_equal test.name, "John Smith"
+        test_user = User.new
+        test_user.name = "John Smith"
+        assert_equal test_user.name, "John Smith"
+    end
+
+    test "requires username" do
+        test_user = User.new
+        assert_not test_user.valid?
+    end
+
+    test "requires password" do
+        test_user = User.new(name: "John Smith")
+        assert_not test_user.valid?
     end
 
 end
