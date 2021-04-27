ecfa0bcfdd472f90f3609e67a3c2557fffd96f63
Shilcof <robert.shilcof@gmail.com>
2021-04-27 20:05:19 +0100

    adds email uniqueness test to user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,13 +9,19 @@ class UserTest < ActiveSupport::TestCase
     end
 
     test "requires username" do
-        test_user = User.new
+        test_user = User.new(password: "Password123")
         assert_not test_user.valid?
     end
 
     test "requires password" do
         test_user = User.new(name: "John Smith")
         assert_not test_user.valid?
+    end
+
+    test "email is invalid when non-unique" do
+      User.create(name: "John Smith", email: "john@gmail.com", password: "Password123")
+      test_user = User.new(name: "John Smith", email: "john@gmail.com", password: "Password123")
+      assert_not test_user.valid?
     end
 
 end
