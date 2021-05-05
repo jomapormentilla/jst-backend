da14c780be9a1c7f2ffef13674acf15ad8b29981
jerry <brownjer3@gmail.com>
2021-05-05 15:21:38 -0700

    add validation that requires commits to be an integer

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
     belongs_to :week
 
-    # validates :size, inclusion: { in: %w(small medium large),
-    #     message: "%{value} is not a valid size" }
+    # validations
+
 end
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,4 +1,7 @@
 class Week < ApplicationRecord
     belongs_to :user
     has_many :activities
+
+    #validations
+    validates :commits, numericality: { only_integer: true }
 end
