1e0857652af9a88e6f29d2764a843cf3d27364f1
jerry <brownjer3@gmail.com>
2021-05-05 15:02:00 -0700

    format changes for readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
     has_secure_password
 
     # validations
-    validates :name, presence: true
-    validates :email, uniqueness: true
-    validates :password, presence: true, on: :create
-    validates :account_type, inclusion: { in: ["Student/graduate", "Career Coach", "Admin"]}
+    validates :name, 
+        presence: true
+    validates :email, 
+        uniqueness: true
+    validates :password,
+        presence: true,
+        on: :create
+    validates :account_type, 
+        presence: true, 
+        inclusion: { in: ["Student/graduate", "Career Coach", "Admin"]}
 
 end
