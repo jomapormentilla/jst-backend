f36ead46c5bd759828a90e1c3f16b65d8f8359bb
jerry <brownjer3@gmail.com>
2021-05-05 14:41:39 -0700

    add validations to check presence of user name and email

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,6 @@
 class Activity < ApplicationRecord
     belongs_to :week
+
+    # validates :size, inclusion: { in: %w(small medium large),
+    #     message: "%{value} is not a valid size" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :weeks
 
     has_secure_password
+
+    # validations
+    validates :name, :email, presence: true
+
+
 end
